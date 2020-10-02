7097d20629a54e090faf3aa4c8d7feeb43935f4d
miroslav-opiela <miroslav-opiela@upjs.sk>
2020-10-02 04:36:57 +0200

    recycler view

---
+++ app/src/main/java/sk/itsovy/android/weblink/MainActivity.java
@@ -0,0 +1,23 @@
+package sk.itsovy.android.weblink;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        WeblinksAdapter adapter = new WeblinksAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+}
